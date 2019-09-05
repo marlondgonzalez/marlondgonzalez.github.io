499ddaf2278433422cb93ed44874e8775ac8dc5e
marlondgonzalez <48197054+marlondgonzalez@users.noreply.github.com>
2019-09-04 22:02:48 -0700

    Made big changes to site

---
+++ script.py
@@ -0,0 +1,2 @@
+<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item" data-aos="fade" data-src="images/portraits/DSC_0417.jpg" data-sub-html="<h4>Fading Light</h4><p>testtext</p>">
+  <a href="#"><img src="images/portraits/DSC_0417.jpg" alt="IMage" class="img-fluid"></a>
